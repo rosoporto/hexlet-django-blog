762d2aed3d8d51bc4121e1dbda21c589653a1c35
rosoporto <rosoporto@yandex.ru>
2024-09-17 21:02:41 +0300

    fix error after stop server

--- hexlet_django_blog/server.py
+++ hexlet_django_blog/server.py
@@ -1,7 +1,15 @@
 import subprocess
+import signal
+import sys
+
+
+def signal_handler(sig, frame):
+    print(' You pressed Ctrl+C!')
+    sys.exit(0)
 
 
 def main():
+    signal.signal(signal.SIGINT, signal_handler)
     cmd = ["python", "manage.py", "runserver", "127.0.0.1:8000"]
     subprocess.run(cmd)
 
